ae8123f7d2a43a20b08da09ca0c20917c52c85a1
Leonardo Porto <leokporto@gmail.com>
2022-05-16 11:57:02 -0300

    alteracoes commited

--- express/public/js/indexTest.js
+++ express/public/js/indexTest.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-(function (){
-=======
+
 'use strict';
 (function() {
->>>>>>> ec3faf0e4ea56f31aa414b9430f923c12049f542
     const ShowMessage = function(message) {
         console.log('Entrou');
         const divMsg = document.getElementById("divMessage");
@@ -13,8 +10,4 @@
     };
     
     ShowMessage("Ola do JS!");
-<<<<<<< HEAD
 })();
-=======
-})();
->>>>>>> ec3faf0e4ea56f31aa414b9430f923c12049f542
