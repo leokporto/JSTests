fe752c9faef2593f9f2c1ed2d1aab2cb7cba844b
Leonardo Porto <leokporto@gmail.com>
2022-05-16 11:53:00 -0300

    alterado script para execucao auto

--- express/public/js/indexTest.js
+++ express/public/js/indexTest.js
@@ -1,9 +1,11 @@
-const ShowMessage = function(message) {
-    console.log('Entrou');
-    const divMsg = document.getElementById("divMessage");
-    divMsg.innerText = message;
-    divMsg.style.color = "#FF0000";
-    console.log('saiu');
-};
-
-ShowMessage("Ola do JS!");
+(function (){
+    const ShowMessage = function(message) {
+        console.log('Entrou');
+        const divMsg = document.getElementById("divMessage");
+        divMsg.innerText = message;
+        divMsg.style.color = "#FF0000";
+        console.log('saiu');
+    };
+    
+    ShowMessage("Ola do JS!");
+})();
